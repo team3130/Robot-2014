77c9dac7d38fca68688008782195b45e1911c976
Matthew Ickstadt <matthew.ickstadt@gmail.com>
2014-01-12 12:28:33 -0600

    Initial Robotbuilder Export
    Mostly Whitespace Changes.

--- OI.cpp
+++ OI.cpp
@@ -1,5 +1,4 @@
 #include "OI.h"
-
 OI::OI() {
 	// Process operator interface input here.
 }
--- OI.h
+++ OI.h
@@ -1,13 +1,9 @@
 #ifndef OI_H
 #define OI_H
-
 #include "WPILib.h"
-
 class OI {
 private:
-
 public:
 	OI();
 };
-
 #endif
