6156f0271a16d1854052ef7385428e86a771ef5b
Mikhail Kyraha <mikhail@kyraha.com>
2014-01-26 21:27:20 -0600

    Copyright/License header comment
    So from now on the code is officially open source and can legally be
    reused in the future seasons. Please try to preserve and maintain these
    headers in your code. Pay a special attention when you merge your
    branches in if they were forked off prior to this revision.

--- Commands/JoystickTank.h
+++ Commands/JoystickTank.h
@@ -1,3 +1,9 @@
+/*-------------------------------------------------------------------------*/
+/* Copyright (c) 2014 FRC-3130 "ERROR 3130". All Rights Reserved.          */
+/* Open Source Software - may be modified, shared, used and reused by FRC  */
+/* teams under the same license as the WPILib code itself.                 */
+/* Authors: Ashwin Chetty                                                  */
+/*-------------------------------------------------------------------------*/
 #ifndef EXAMPLE_COMMAND_H
 #define EXAMPLE_COMMAND_H
 
--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -1,3 +1,9 @@
+/*-------------------------------------------------------------------------*/
+/* Copyright (c) 2014 FRC-3130 "ERROR 3130". All Rights Reserved.          */
+/* Open Source Software - may be modified, shared, used and reused by FRC  */
+/* teams under the same license as the WPILib code itself.                 */
+/* Authors: Matthew Ickstadt                                               */
+/*-------------------------------------------------------------------------*/
 #include "Logger.h"
 #include "Driverstation.h"
 #include "stat.h"
