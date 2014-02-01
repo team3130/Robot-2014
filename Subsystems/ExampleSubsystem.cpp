92bb17f8ed2957d783386003f9af7431637b6c30
The-Dawwctor <jamesemc2@gmail.com>
2014-02-01 14:28:44 -0600

    Added Licensing to Example Code
    Realized that if new commands are made, probably should include the
    licensing so that you can just substitute authors if you copy the code
    format.

--- Subsystems/ExampleSubsystem.cpp
+++ Subsystems/ExampleSubsystem.cpp
@@ -1,3 +1,10 @@
+/*-------------------------------------------------------------------------*/
+/* Copyright (c) 2014 FRC-3130 "ERROR 3130". All Rights Reserved.          */
+/* Open Source Software - may be modified, shared, used and reused by FRC  */
+/* teams under the same license as the WPILib code itself.                 */
+/* Authors: <INSERT AUTHOR(S) OF CODE>                                     */
+/*-------------------------------------------------------------------------*/
+
 #include "ExampleSubsystem.h"
 #include "../Robotmap.h"
 
