8f46f476f0dbb1ef2fc4d8f53523e92d988b3141
Ashwin <ashwinchetty@gmail.com>
2014-02-02 18:28:29 -0600

    functional and quick PID for position/rotation
    PID Tuning:
    
    If   Pulses/Distance = 3000
    Then 1 UNIT = 8.64 feet.
    = (3000/360)
    
    For PID Rotate:
    P = -7.00
    I = 0.0
    D = 0.0
    
    For PID Straight:
    P = -3000
    I = 0.0
    D = 0.0
    
    For Chassis PID, BOTH SIDES:
    P = 0.0
    I = 1000.0
    D = 0.0

--- Commands/Rotate.cpp
+++ Commands/Rotate.cpp
@@ -6,7 +6,7 @@
 /*-------------------------------------------------------------------------*/
 
 #include "Rotate.h"
-<<<<<<< HEAD
+
 #include "math.h"
 #include "../DoubleEncoder.h"
 Rotate::Rotate(double dist, double thresh, double timeToWait, double p, double i, double d): PIDCommand("Rotate", p, i, d){
@@ -20,19 +20,10 @@ Rotate::Rotate(double dist, double thresh, double timeToWait, double p, double i
 	SmartDashboard::PutNumber("Rotate PID P",-10000);
 	SmartDashboard::PutNumber("Rotate PID I",0);
 	SmartDashboard::PutNumber("Rotate PID D",0);
-	SmartDashboard::PutNumber("RotateGoal",0.5);
-=======
-
-Rotate::Rotate(double angle, double p, double i, double d)
-: PIDCommand("Rotate", p, i, d)
-{
-	Requires(CommandBase::chassis);
-	SmartDashboard::PutData(this);
->>>>>>> 47bea4b9c9d7b12b7e1199060397fac8ff3aacdd
+	SmartDashboard::PutNumber("RotateGoal",90);
 }
 // Called just before this Command runs the first time
 void Rotate::Initialize() {
-<<<<<<< HEAD
 	double np=SmartDashboard::GetNumber("Rotate PID P")/1000.;
 	double ni=SmartDashboard::GetNumber("Rotate PID I")/1000.;
 	double nd=SmartDashboard::GetNumber("Rotate PID D")/1000.;
@@ -43,24 +34,21 @@ void Rotate::Initialize() {
 	chassis->gyro->SetPIDSourceParameter(PIDSource::kAngle);
 	timer.Reset();
 	timer.Start();
+	controller->Reset();
+	controller->Enable();
 	chassis->resetBias();
-=======
-	GetPIDController()->Reset();
-	GetPIDController()->SetAbsoluteTolerance(SmartDashboard::GetNumber("Rotate Tolerance"));
-	SetSetpoint(SmartDashboard::GetNumber("Rotate"));
-	GetPIDController()->SetPID(
-		SmartDashboard::GetNumber("Rotate PID P"),
-		SmartDashboard::GetNumber("Rotate PID I"),
-		SmartDashboard::GetNumber("Rotate PID D"));
->>>>>>> 47bea4b9c9d7b12b7e1199060397fac8ff3aacdd
+	chassis->leftEncoder->Reset();
+	chassis->rightEncoder->Reset();
+	//GetPIDController()->Reset();
 }
 
 // Called repeatedly when this Command is scheduled to run
 void Rotate::Execute() {
-<<<<<<< HEAD
-	double dist = goal-(chassis->leftEncoder->GetDistance()+chassis->rightEncoder->GetDistance())/2.0;
+	double dist = goal-(chassis->gyro->GetAngle());
 	distanceToGoal=dist;
 	SmartDashboard::PutNumber("Gyro D",chassis->gyro->GetAngle());
+	SmartDashboard::PutNumber("Encoder L",chassis->leftEncoder->Get());
+	SmartDashboard::PutNumber("Encoder R",chassis->rightEncoder->Get());
 	//(4x|x|)/(4x^2+1)
 	//This function has no mathematical significance; but because it tapers off near 0, it
 	//is preferable to a purely proportional control. This function also has horizontal asymptotes
@@ -71,14 +59,10 @@ void Rotate::Execute() {
 	//if(power<-1)power=-1;
 	//if(power>1)power=1;
 	//chassis->straightDrive(power);
-=======
-	SmartDashboard::PutNumber("Rotate to go",GetPIDController()->GetError());
->>>>>>> 47bea4b9c9d7b12b7e1199060397fac8ff3aacdd
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool Rotate::IsFinished() {
-<<<<<<< HEAD
 	if(fabs(distanceToGoal)<threshold){
 		timer.Start();
 	}else{
@@ -87,17 +71,24 @@ bool Rotate::IsFinished() {
 	}
 	if(timer.Get()>=confirmTime)return true;
 	else return false;
-=======
-	return GetPIDController()->OnTarget();
->>>>>>> 47bea4b9c9d7b12b7e1199060397fac8ff3aacdd
 }
 double Rotate::ReturnPIDInput(){
-	//double gyroInput = chassis->gyro->Get
 	return chassis->gyro->GetAngle();
 }
 void Rotate::UsePIDOutput(double output){
-	if(fabs(output)>1)controller->Reset();	//i component is too high, or something.
+	static double lastOutput=0;
+	if(output*lastOutput<0){
+//		controller->Reset();
+//		controller->Enable();
+	}
+	
+	if(output<0.11 && output >0.01)output=0.11;
+	if(output>-0.11 && output <-0.01)output=-0.11;
+	if(output<-.5)output=-.5;
+	if(output>.5)output=0.5;
+	
 	chassis->tankDrive(output,-output);
+	lastOutput=output;
 }
 
 // Called once after isFinished returns true
@@ -109,12 +100,4 @@ void Rotate::End() {
 // subsystems is scheduled to run
 void Rotate::Interrupted() {
 	
-}
-double Rotate::ReturnPIDInput(){
-	double nowAngle = CommandBase::chassis->gyro->GetAngle();
-	double normalizer = SmartDashboard::GetNumber("Rotate Slowdown");
-	return normalizer != 0.0 ? nowAngle / normalizer : nowAngle;
-}
-void Rotate::UsePIDOutput(double output){
-	CommandBase::chassis->tankDrive(output,-output);
 }
--- OI.h
+++ OI.h
@@ -15,11 +15,7 @@ public:
 	JoystickButton* rightPrecision;
 	JoystickButton* straightMode;
 	JoystickButton* driveTest;
-<<<<<<< HEAD
 	JoystickButton* rotateTest;
-=======
-	JoystickButton* btnTest2;
->>>>>>> 47bea4b9c9d7b12b7e1199060397fac8ff3aacdd
 };
 
 #endif
