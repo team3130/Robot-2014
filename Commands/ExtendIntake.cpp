52ba2bd0a50c6ca07b9e0628bec1f1870ee0f737
The-Dawwctor <jamesemc2@gmail.com>
2014-02-09 17:48:03 -0600

    minor changes: unimportant
    Really not important.

--- Commands/ExtendIntake.cpp
+++ Commands/ExtendIntake.cpp
@@ -23,7 +23,7 @@ void ExtendIntake::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool ExtendIntake::IsFinished() {
-	return false;
+	return true;
 }
 
 // Called once after isFinished returns true
--- Commands/SpinIntake.cpp
+++ Commands/SpinIntake.cpp
@@ -25,7 +25,7 @@ void SpinIntake::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool SpinIntake::IsFinished() {
-	return false;
+	return true;
 }
 
 // Called once after isFinished returns true
