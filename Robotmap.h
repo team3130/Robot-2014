65c7844438489f68576612e7eb0130d5d0d6c9a0
Mikhail Kyraha <mikhail@kyraha.com>
2014-02-13 16:11:42 -0600

    Ports assigned (not final)

--- Robotmap.h
+++ Robotmap.h
@@ -19,31 +19,41 @@
 // following variables to use with your drivetrain subsystem.
 
 // It is convention to name all #defined constants with all caps names.
-#define C_PRESSURE_SWITCH 0
-#define C_COMPRESSOR_RELAY 0
-#define C_WINCHMOTOR1 3
-#define C_SHOOTERSOLENOID1 4
-#define C_INTAKEMOTOR1 5
-#define C_EXTENDSOLENOID1 6
-#define C_ENCODER_WINCH_CHANNEL_1 5
-#define C_ENCODER_WINCH_CHANNEL_2 6
+
+// Digital Input channels
+#define C_PRESSURE_SWITCH 9
+#define C_ENCODER_WINCH_CHANNEL_1  5
+#define C_ENCODER_WINCH_CHANNEL_2  6
 #define C_ENCODER_INTAKE_CHANNEL_1 7
 #define C_ENCODER_INTAKE_CHANNEL_2 8
+#define C_ENCODER_LEFT_A  3
+#define C_ENCODER_LEFT_B  4
+#define C_ENCODER_RIGHT_A 1
+#define C_ENCODER_RIGHT_B 2
+
+// Analog Input channels
+#define C_GYRO 1
+
+// Relays and solenoids
+#define C_COMPRESSOR_RELAY 1
+#define C_SHOOTERSOLENOID1 4
+#define C_EXTENDSOLENOID1 6
+#define C_SHIFTER 2
+
+// Motors (Jag and Talons)
+#define C_WINCHMOTOR1 3
+#define C_INTAKEMOTOR1 5
 #define C_LEFTMOTOR 2
 #define C_RIGHTMOTOR 1
 #define C_LEFTSATELLITE 4
 #define C_RIGHTSATELLITE 3
-#define C_ENCODER_LEFT_A 3
-#define C_ENCODER_LEFT_B 4
-#define C_ENCODER_RIGHT_A 1
-#define C_ENCODER_RIGHT_B 2
-#define C_GYRO 1
-#define C_SHIFTER 0
-#define C_SHIFTER_MODULE 1
+
 
 // If you are using multiple modules, make sure to define both the port
 // number and the module. For example you with a rangefinder:
 // #define RANGE_FINDER_PORT 1
 // #define RANGE_FINDER_MODULE 1
+
+//#define C_SHIFTER_MODULE 1
 
 #endif
