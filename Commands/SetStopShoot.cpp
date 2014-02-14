5010296f39b3d75f5cc77ac699392c9e11c644b1
Superkb10 <superkevinbi@gmail.com>
2014-02-14 17:56:58 -0600

    Set Safety for ShootCatapult
    Made sure that the intake bar is out of the way of the catapult before
    shooting.

--- Commands/SetStopShoot.cpp
+++ Commands/SetStopShoot.cpp
@@ -2,45 +2,39 @@
 /* Copyright (c) 2014 FRC-3130 "ERROR 3130". All Rights Reserved.          */
 /* Open Source Software - may be modified, shared, used and reused by FRC  */
 /* teams under the same license as the WPILib code itself.                 */
-/* Authors: James Li, Kevin Bi                                             */
+/* Authors: <Kevin Bi>                                                     */
 /*-------------------------------------------------------------------------*/
 
-#include "ShootCatapult.h"
+#include "SetStopShoot.h"
 
-ShootCatapult::ShootCatapult() {
+SetStopShoot::SetStopShoot() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(chassis);
-	Requires(shooter, intake);
+	Requires(shooter); 
 }
 
 // Called just before this Command runs the first time
-void ShootCatapult::Initialize() 
-{
-	intake->ExtendArms(false);
-	intake->setIdle(true);
-	shooter->setPinch(false);
+void SetStopShoot::Initialize() {
+	
 }
 
 // Called repeatedly when this Command is scheduled to run
-void ShootCatapult::Execute() {
-	if(shooter->getPinch1() == false && shooter->getPinch2() == true)
-	{
-		Finished = true;
-	}
+void SetStopShoot::Execute() {
+	
 }
 
 // Make this return true when this Command no longer needs to run execute()
-bool ShootCatapult::IsFinished() {
-	return Finished; 
+bool SetStopShoot::IsFinished() {
+	return false;
 }
 
 // Called once after isFinished returns true
-void ShootCatapult::End() {
-	shooter->setReady(false);
+void SetStopShoot::End() {
+	
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void ShootCatapult::Interrupted() {
+void SetStopShoot::Interrupted() {
 	
 }
---
+++ Commands/SetStopShoot.h
@@ -0,0 +1,30 @@
+/*-------------------------------------------------------------------------*/
+/* Copyright (c) 2014 FRC-3130 "ERROR 3130". All Rights Reserved.          */
+/* Open Source Software - may be modified, shared, used and reused by FRC  */
+/* teams under the same license as the WPILib code itself.                 */
+/* Authors: <Kevin Bi>                                                     */
+/*-------------------------------------------------------------------------*/
+
+#ifndef SET_STOP_SHOOT_H
+#define SET_STOP_SHOOT_H
+
+#include "../CommandBase.h"
+
+/**
+ *
+ *
+ * @author ExampleAuthor
+ */
+class SetStopShoot: public CommandBase {
+private:
+	static const double Position = 0; 
+public:
+	SetStopShoot();
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+};
+
+#endif
