5026f644741bf4096eb20b9e26cef3f593b7f276
error3130laptop <ashwinchetty@gmail.com>
2014-02-13 23:00:13 -0600

    Channels mapped

--- Robotmap.h
+++ Robotmap.h
@@ -21,15 +21,13 @@
 // It is convention to name all #defined constants with all caps names.
 
 // Digital Input channels
-#define C_PRESSURE_SWITCH 9
-#define C_ENCODER_WINCH_CHANNEL_1  5
-#define C_ENCODER_WINCH_CHANNEL_2  6
-#define C_ENCODER_STOPPER_A 10
-#define C_ENCODER_STOPPER_B 11
-#define C_ENCODER_ARM_A 12
-#define C_ENCODER_ARM_B 13
-#define C_ENCODER_INTAKE_CHANNEL_1 7
-#define C_ENCODER_INTAKE_CHANNEL_2 8
+#define C_PRESSURE_SWITCH 14 
+#define C_ENCODER_WINCH_CHANNEL_1  9
+#define C_ENCODER_WINCH_CHANNEL_2  10
+#define C_ENCODER_STOPPER_A 5
+#define C_ENCODER_STOPPER_B 6
+#define C_ENCODER_CATAPULT_A 7
+#define C_ENCODER_CATAPULT_B 8
 #define C_ENCODER_LEFT_A  3
 #define C_ENCODER_LEFT_B  4
 #define C_ENCODER_RIGHT_A 1
@@ -38,28 +36,31 @@
 // Analog Input channels
 #define C_GYRO 1
 
-// Relays and solenoids
+// Relays
 #define C_COMPRESSOR_RELAY 1
+
+// Solenoids
 #define C_SHOOTERSOLENOID1 4
 #define C_SHOOTERSOLENOID2 5
-#define C_EXTENDSOLENOID1 6
-#define C_IDLESOLENOID 3
-#define C_SHIFTER 2
+#define C_EXTENDSOLENOID1 2
+#define C_IDLESOLENOID 1
+#define C_SHIFTER 3
 
-// Motors (Jag and Talons)
-#define C_WINCHMOTOR1 3
-#define C_STOPPERMOTOR 6
-#define C_INTAKEMOTOR1 5
-#define C_LEFTMOTOR 2
-#define C_RIGHTMOTOR 1
-#define C_LEFTSATELLITE 4
-#define C_RIGHTSATELLITE 3
+// PWM Motors (Jag and Talons)
+#define C_WINCHMOTOR1 7
+#define C_STOPPERMOTOR 5
+#define C_INTAKEMOTOR1 6
+#define C_LEFTMOTOR 1
+#define C_RIGHTMOTOR 3
+#define C_LEFTSATELLITE 2
+#define C_RIGHTSATELLITE 4
 
-// Buttons
+// Buttons and axis
+#define B_HIGHGEAR 3
 #define B_EXTENDARMS 4
-#define B_PULLARMS 2
-#define B_IDLEARMS 1
-#define B_BEATERBAR 6
+#define B_PULLARMS 1
+#define B_IDLEARMS 2
+#define A_BEATERBAR 3
 #define B_POWERWINCH 2
 #define B_STOPPERWINCH 5
 #define B_SHOOT 8
