41cbf1cf9bc3a89557af0f5d763498d1c12ee7ed
Superkb10 <superkevinbi@gmail.com>
2014-02-14 16:27:20 -0600

    Created Stopper Set commands
    Created commands to set the location of the stop motor.

---
+++ Commands/SetStopShoot.cpp
@@ -0,0 +1,39 @@
+/*-------------------------------------------------------------------------*/
+/* Copyright (c) 2014 FRC-3130 "ERROR 3130". All Rights Reserved.          */
+/* Open Source Software - may be modified, shared, used and reused by FRC  */
+/* teams under the same license as the WPILib code itself.                 */
+/* Authors: <Kevin Bi>                                                     */
+/*-------------------------------------------------------------------------*/
+
+#include "SetStopShoot.h"
+
+SetStopShoot::SetStopShoot() {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+}
+
+// Called just before this Command runs the first time
+void SetStopShoot::Initialize() {
+	
+}
+
+// Called repeatedly when this Command is scheduled to run
+void SetStopShoot::Execute() {
+	
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool SetStopShoot::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void SetStopShoot::End() {
+	
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void SetStopShoot::Interrupted() {
+	
+}
